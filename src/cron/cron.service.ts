2ad7c8236033f9c86a160fe37a3516c1ba400328
wzdzzz <new1161437123@gmail.com>
2024-10-31 10:51:13 +0800

    feat: 添加定时任务

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 
@@ -6,14 +6,28 @@ import { CronJob } from 'cron';
 export class CronService {
   constructor(private schedulerRegistry: SchedulerRegistry) {}
 
-  addCronJob(name: string, time: string, onTick: () => void): void {
+  addCronJob(name: string, time: string, onTick: () => void) {
     const job = new CronJob(`${time}`, onTick);
 
     this.schedulerRegistry.addCronJob(name, job);
     job.start();
+    return {
+      job: name,
+    };
   }
 
-  deleteCronJob(name: string): void {
-    this.schedulerRegistry.deleteCronJob(name);
+  deleteCronJob(name: string) {
+    try {
+      this.schedulerRegistry.deleteCronJob(name);
+      return {
+        job: name,
+      };
+    } catch (_err) {
+      console.log(_err);
+      throw new HttpException(
+        'No Cron Job was found with the given name. Check that you created one with a decorator or with the create API.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
